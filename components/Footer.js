55a6c74a4034d41282c089e1bc0aa0b011cd6ad5
amanthajayathilake <amanthajayathilake@gmail.com>
2022-01-23 21:07:12 +0530

    config file implementation

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 // import logo from '../images/logo.png';
 
 const Footer = () => (
-  <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer" style={{background:"black"}}>
+  <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
     <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
       {/* <div className="flex flex-[0.5] justify-center items-center">
         <img src={logo} alt="logo" className="w-32" />
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Web3Modal from "web3modal"
 import styles from '../styles/Home.module.css'
+import {
+  nftaddress, 
+  nftmarketaddress
+} from '../config'
 
 export default function Home() {
   return (
